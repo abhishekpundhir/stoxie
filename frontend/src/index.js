e8110d41d50e4a3af38d20da65462aec5e805281
Abhishek Pundhir <theabhishek1o1@gmail.com>
2025-07-31 09:58:24 +0530

    Created a Not found route with its instance

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
+// Navbar Routes
 import HomePage from './landing_page/home/HomePage'
 import About from './landing_page/about/AboutPage'
 import Support from './landing_page/support/SupportPage'
 import Product from './landing_page/products/Hero'
 import Pricing from './landing_page/pricing/PricingPage'
 import SingnupPage from './landing_page/signup/Signup'
+
+// Comman Components Routes
 import Navbar from '../src/landing_page/Navbar';
 import Footer from '../src/landing_page/Footer';
+import NotFound from './landing_page/NotFound'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -22,6 +26,7 @@ root.render(
       <Route path="/product" element={<Product/>}/> 
       <Route path="/pricing" element={<Pricing/>}/> 
       <Route path="/signup" element={<SingnupPage/>}/> 
+      <Route path="*" element={<NotFound/>}/> 
     </Routes>
     < Footer/>
   </BrowserRouter>
--- frontend/src/landing_page/NotFound.js
+++ frontend/src/landing_page/NotFound.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import "../landing_page/stylesheet.css"
+import { Link } from 'react-router-dom';
 function NotFound() {
     return (
         <div className='container p-7 mb-5'>
             <div className='row text-center'>
              {/* <img src='media/img/notfound.png' alt='Hero Image' className='mb-5'/> */}
-             <h1 className='mt-5 mb-2'>Go Borderless With Stoxie </h1>
-             <p className='text-muted'> Zero-cost investing. ₹20 smart trades. One ultra-modern platform.</p>
-             <button className='btn-1 mb-5'>Join Now</button>
+             <h1 className='mt-5 mb-2'>404 Page Not Found </h1>
+             <p className='text-muted'> Sorry, The Page Your Are Looking for is Not Exist</p>
+            <Link to="/"><button className='btn-1 mb-5' >Go Home</button></Link> 
             </div>
         </div>
     ); 
