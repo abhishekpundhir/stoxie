56f80bf846e8bbd8d544f950e36431e2b5b45396
Abhishek Pundhir <theabhishek1o1@gmail.com>
2025-08-09 22:31:01 +0530

    finished the support page

--- frontend/src/landing_page/support/CreateTicket.js
+++ frontend/src/landing_page/support/CreateTicket.js
@@ -1,13 +1,129 @@
-import React from 'react'
+import React from "react";
+import "../stylesheet.css"
+
+
+
 function CreateTicket() {
-    return (
-        <div className='container mt-5 mb-5'>
-            <div className='row p-5'>
-                <h1 className='text-center fs-4'>We Reimagined Broking with Radical Transparency <br /><br />Now, We're Building a Technology-first future for Investors Everywhere.</h1>
-            </div>
+  return (
+    <div className="container">
+      <div className="row p-5 mt-5 mb-5">
+        <h1 className="fs-2 text-center">To create a ticket, select a relevant topic</h1>
+        <div className="col-4 p-5 mt-2 mb-2 alink ">
+          <h4 className="">
+            <i class="fa fa-plus-circle" aria-hidden="true"></i> Account Opening
+          </h4>
+          <a className="text-fade2" href="" style={{ lineHeight: "2.5" }}>
+            Online Account Opening
+          </a>
+          <br />
+          <a className="text-fade2"  href="" style={{ lineHeight: "2.5" }}>
+            Offline Account Opening
+          </a>
+          <br />
+          <a className="text-fade2"  href="" style={{ lineHeight: "2.5" }}>
+            Company, Partnership and HUF Account
+          </a>
+          <br />
+          <a className="text-fade2"  href="" style={{ lineHeight: "2.5" }}>
+            Opening
+          </a>
+          <br />
+          <a className="text-fade2"  href="" style={{ lineHeight: "2.5" }}>
+            NRI Account Opening
+          </a>
+          <br />
+          <a className="text-fade2"  href="" style={{ lineHeight: "2.5" }}>
+            Charges at Stoxie
+          </a>
+          <br />
+          <a className="text-fade2"  href="" style={{ lineHeight: "2.5" }}>
+            Stoxie IDFC FIRST Bank 3-in-1 Account
+          </a>
+          <br />
+          <a  className="text-fade2" href="" style={{ lineHeight: "2.5" }}>
+            Economic calendar
+          </a>
+          <br />
+        </div>
+        <div className="col-4 p-5 mt-2 mb-2 alink">
+          <h4 className="">
+            <i class="fa fa-plus-circle" aria-hidden="true"></i> For Tracking
+          </h4>
+          <a  className="text-fade2" href="" style={{  lineHeight: "2.5" }}>
+            Contact us
+          </a>
+          <br />
+          <a className="text-fade2"  href="" style={{  lineHeight: "2.5" }}>
+            Support portal
+          </a>
+          <br />
+          <a  className="text-fade2" href="" style={{  lineHeight: "2.5" }}>
+            How to file a complaint?
+          </a>
+          <br />
+          <a className="text-fade2"  href="" style={{  lineHeight: "2.5" }}>
+            Status of your complaints
+          </a>
+          <br />
+          <a className="text-fade2"  href="" style={{  lineHeight: "2.5" }}>
+            Circulars
+          </a>
+          <br />
+          <a  className="text-fade2" href="" style={{  lineHeight: "2.5" }}>
+            Z-Connect blog
+          </a>
+          <br />
+          <a  className="text-fade2" href="" style={{  lineHeight: "2.5" }}>
+            Bulletin
+          </a>
+          <br />
+          <a  className="text-fade2" href="" style={{ textDecoration: "none", lineHeight: "2.5" }}>
+            Brokerage charges
+          </a>
+          <br />
+        </div>
+        <div className="col-4 p-5 mt-2 mb-2 alink">
+          <h4 className="">
+            <i class="fa fa-plus-circle" aria-hidden="true"></i> Stoxie Coorporations
+          </h4>
+          <a  className="text-fade2" href="/about" style={{  lineHeight: "2.5" }}>
+            About
+          </a>
+          <br />
+          <a  className="text-fade2" href="" style={{  lineHeight: "2.5" }}>
+            Philosophy
+          </a>
+          <br />
+          <a  className="text-fade2" href="" style={{  lineHeight: "2.5" }}>
+            Careers
+          </a>
+          <br />
+          <a  className="text-fade2" href="" style={{  lineHeight: "2.5" }}>
+            Open source
+          </a>
+          <br />
+          <a  className="text-fade2" href="" style={{  lineHeight: "2.5" }}>
+            Holdings
+          </a>
+          <br />
+          <a  className="text-fade2" href="" style={{  lineHeight: "2.5" }}>
+            Stoxie Collaborations
+          </a>
+          <br />
+          <a  className="text-fade2" href="" style={{  lineHeight: "2.5" }}>
+            Stoxie betaverse
+          </a>
+          <br />
+          <a className="text-fade2"  href="" style={{  lineHeight: "2.5" }}>
+            Upcoming IPOs
+          </a>
+          <br />
         </div>
 
-    );
+       
+      </div>
+    </div>
+  );
 }
 
 export default CreateTicket;
--- frontend/src/landing_page/support/Hero.js
+++ frontend/src/landing_page/support/Hero.js
@@ -1,7 +1,44 @@
 import React from 'react'
 function Hero() {
-    return (  
-       <h1>Hero</h1>
+    return (
+        <div id="carouselExampleCaptions" class="carousel slide">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="media\img\card3.jpg"  class="d-block w-100" alt="..."/>
+      <div class="carousel-caption d-none d-md-block">
+        <h2>Support Portal</h2>
+        <h3>Stoxie Team Online for Anykind of Help.</h3>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="media\img\card3.jpg" class="d-block w-100" alt="..."/>
+      <div class="carousel-caption d-none d-md-block">
+        <h2>Features</h2>
+        <h3>Zero-cost investing. ₹20 smart trades. One ultra-modern platform.</h3>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="media\img\card3.jpg" class="d-block w-100" alt="..."/>
+      <div class="carousel-caption d-none d-md-block">
+        <h2>Stoxie Service</h2>
+        <h3>At Stoxie, we believe that knowledge is as valuable as capital.</h3>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    {/* <span class="carousel-control-prev-icon" aria-hidden="true"></span> */}
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    {/* <span class="carousel-control-next-icon" aria-hidden="true"></span> */}
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
     );
 }
 
